87d5200ee3be9bbf4f5e9984c253d0bc1e9ffd3a
Croqueteer <davidasmith@gmail.com>
2021-12-04 23:31:25 -0500

    [Microverse] Update avatar
    Mouse wheel to move camera behind avatar.
    Updated sky.

--- src/index.js
+++ src/index.js
@@ -7,7 +7,9 @@
 
 import { App,  ModelRoot, ViewRoot, StartWorldcore, Actor, Pawn, mix, InputManager, PlayerManager,
     AM_Player, PM_Player, PM_ThreeVisible, ThreeRenderManager, AM_Spatial, PM_Spatial, AM_Avatar, 
-    PM_Avatar, PM_ThreeCamera, toRad, v3_transform, v3_sqrMag, v3_sub, v3_add, q_identity, q_euler, m4_rotationQ} from "@croquet/worldcore";
+    PM_Avatar, PM_ThreeCamera, toRad, 
+    v3_transform, v3_sqrMag, v3_sub, v3_add, q_identity, q_euler, q_axisAngle,
+    m4_multiply, m4_rotationQ, m4_translation} from "@croquet/worldcore";
 
 import * as THREE from './three/build/three.module.js';
 import { GLTFLoader } from './three/examples/jsm/loaders/GLTFLoader.js';
@@ -18,7 +20,7 @@ import JSZip from "jszip";
 
 // model art CC 4.0 https://sketchfab.com/3d-models/substation-bimfra-37528b7d65f945d0b31389d95abced6d
 const powerPlant = "../assets/OilFacility.glb.zip";
-const simplehead = "../assets/simplehead.glb.zip";
+
 const alice  = "../assets/avatars/alice.zip";
 const cheshire = "../assets/avatars/cheshirecat.zip";
 const hatter = "../assets/avatars/madhatter.zip";
@@ -27,12 +29,12 @@ const queen = "../assets/avatars/queenofhearts.zip";
 const rabbit = "../assets/avatars/whiterabbit.zip";
 
 // skybox art courtesy of https://opengameart.org/users/spiney
-import skyFront from "../assets/sky/bluecloud_ft.jpg";
-import skyBack from "../assets/sky/bluecloud_bk.jpg";
-import skyRight from "../assets/sky/bluecloud_rt.jpg";
-import skyLeft from "../assets/sky/bluecloud_lf.jpg";
-import skyUp from "../assets/sky/bluecloud_up.jpg";
-import skyDown from "../assets/sky/bluecloud_dn.jpg";
+import skyFront from "../assets/sky/sh_ft.png";
+import skyBack from "../assets/sky/sh_bk.png";
+import skyRight from "../assets/sky/sh_rt.png";
+import skyLeft from "../assets/sky/sh_lf.png";
+import skyUp from "../assets/sky/sh_up.png";
+import skyDown from "../assets/sky/sh_dn.png";
 
 console.log('%cTHREE.REVISION:', 'color: #f00', THREE.REVISION);
 console.log("%cJSZip.Version",  'color: #f00', JSZip.version);
@@ -95,21 +97,18 @@ function addShadows(gltf, layer) {
 }
 
 // these are defined outside of the Worldcore objects, otherwise, they will need to be recreated when the app goes to sleep and restarts again.
-
-const avatar = new THREE.Group(); // note that we are putting avatars into layer 2
-loadGLB(simplehead, "simplehead.glb", avatar, addShadows, [0, -0.2, 0.0], [0.4,0.4,0.4], [0, Math.PI, 0], 2);
-
 const plant = new THREE.Group();
 loadGLB(powerPlant, "OilFacility.glb", plant, addShadows, [0, -10, 0], [1,1,1], [0,0,0], 1);
 
-const a1 = new THREE.Group(); loadGLB(alice, "alice.glb", a1, addShadows, [0,-0.2,0], [0.4, 0.4, 0.4], [0, Math.PI, 0], 2);
-const a2 = new THREE.Group(); loadGLB(hatter, "madhatter.glb", a2, addShadows, [0,-0.2,0], [0.4, 0.4, 0.4], [0, Math.PI, 0], 2);
-const a3 = new THREE.Group(); loadGLB(hare, "march.glb", a3, addShadows, [0,-0.2,0], [0.4, 0.4, 0.4], [0, Math.PI, 0], 2);
-const a4 = new THREE.Group(); loadGLB(queen, "queenofhearts.glb", a4, addShadows, [0,-0.2,0], [0.4, 0.4, 0.4], [0, Math.PI, 0], 2);
-const a5 = new THREE.Group(); loadGLB(rabbit, "white.glb", a5, addShadows, [0,-0.2,0], [0.4, 0.4, 0.4], [0, Math.PI, 0], 2);
-const a6 = new THREE.Group(); loadGLB(cheshire, "cheshirecat.glb", a6, addShadows, [0,-0.2,0], [0.4, 0.4, 0.4], [0, Math.PI, 0], 2);
-
-const avatars = [a1, a2, a3, a4, a5, a6];
+var i = 0;
+const avatars = []; for(i=0; i<6;i++) avatars[i]=new THREE.Group;
+i=0;
+loadGLB(alice, "alice.glb", avatars[i++], addShadows, [0,-0.2,0], [0.4, 0.4, 0.4], [0, Math.PI, 0], 2);
+loadGLB(rabbit, "white.glb", avatars[i++], addShadows, [0,-0.2,0], [0.4, 0.4, 0.4], [0, Math.PI, 0], 2);
+loadGLB(hatter, "madhatter.glb", avatars[i++], addShadows, [0,-0.2,0], [0.4, 0.4, 0.4], [0, Math.PI, 0], 2);
+loadGLB(hare, "march.glb", avatars[i++], addShadows, [0,-0.2,0], [0.4, 0.4, 0.4], [0, Math.PI, 0], 2);
+loadGLB(queen, "queenofhearts.glb", avatars[i++], addShadows, [0,-0.2,0], [0.4, 0.4, 0.4], [0, Math.PI, 0], 2);
+loadGLB(cheshire, "cheshirecat.glb", avatars[i++], addShadows, [0,-0.2,0], [0.4, 0.4, 0.4], [0, Math.PI, 0], 2);
 
 class MyAvatar extends mix(Actor).with(AM_Player, AM_Avatar) {
     init(options) {
@@ -117,6 +116,14 @@ class MyAvatar extends mix(Actor).with(AM_Player, AM_Avatar) {
         super.init(options);
     }
     get pawn() {return AvatarPawn}
+    
+    get lookPitch() { return this._lookPitch || 0 };
+    get lookYaw() { return this._lookYaw || 0 };
+
+    onLookTo(e) {
+        this.set({lookPitch: e[0], lookYaw: e[1]});
+        this.rotateTo(q_euler(0, this.lookYaw, 0));
+    }
 }
 
 MyAvatar.register('MyAvatar');
@@ -129,6 +136,10 @@ class AvatarPawn extends mix(Pawn).with(PM_Player, PM_Avatar, PM_ThreeVisible, P
         this.opacity = 1;
         this.activeMMotion = false; // mobile motion initally inactive
         this.avatarIndex = args[0].avatarIndex;
+        this._lookPitch = this.actor.lookPitch;
+        this._lookYaw = this.actor.lookYaw;
+        this._lookOffset = [0,0,0]; // Vector displacing the camera from the avatar origin.
+
         if (this.isMyPlayerPawn) {
             myAvatar = this; // set the global for callbacks
             // create a dummy camera that will be moved by the OrbitControls
@@ -136,11 +147,14 @@ class AvatarPawn extends mix(Pawn).with(PM_Player, PM_Avatar, PM_ThreeVisible, P
             this.camera = renderMgr.camera;
             this.scene = renderMgr.scene;
             this.lastHeight = eyeHeight; // tracking the height above ground
-            this.tweenCamera = new THREE.PerspectiveCamera(60, window.innerWidth / window.innerHeight, 0.1, 10000);
-            this.walkCamera = new THREE.PerspectiveCamera(60, window.innerWidth / window.innerHeight, 0.1, 10000);
+
+            //this.tweenCamera = new THREE.PerspectiveCamera(60, window.innerWidth / window.innerHeight, 0.1, 10000);
+            //this.walkCamera = new THREE.PerspectiveCamera(60, window.innerWidth / window.innerHeight, 0.1, 10000);
             this.orbitCamera = new THREE.PerspectiveCamera(60, window.innerWidth / window.innerHeight, 0.1, 10000);
+            this.tweenCamera = new THREE.Object3D();
+            this.walkCamera = new THREE.Object3D();
+            //this.orbitCamera = new THREE.Object3D();
             this.orbitCamera.position.set( 100, 50, 0 );
-            //this.orbitCamera.updateMatrixWorld();
 
             this.createOrbitControls( this.orbitCamera, renderMgr.renderer );
             this.setControls(isWalking); // walking or orbiting?
@@ -148,15 +162,29 @@ class AvatarPawn extends mix(Pawn).with(PM_Player, PM_Avatar, PM_ThreeVisible, P
             this.subscribe("input", "pointerUp", this.endMMotion);
             this.subscribe("input", "pointerCancel", this.endMMotion);
             this.subscribe("input", "pointerMove", this.continueMMotion);
+            this.subscribe("input", "wheel", this.thirdPerson);
             this.raycaster = new THREE.Raycaster( new THREE.Vector3(), new THREE.Vector3( 0, - 1, 0 ), 0, 10 );
             this.raycaster.layers.set( 1 ); // only test against layer 1. Layer 2 is other players.
             this.future(100).fadeNearby();
-        } else { 
+        } //else { 
             // create the avatar (cloned from above) for anyone that is not me (for now)
             let a = this.avatar = avatars[this.avatarIndex%avatars.length];
             a.traverse( n => {if(n.material)n.material = n.material.clone();});
             this.setRenderObject(a);
-        }
+        //}
+    }
+
+    get lookOffset(){return this._lookOffset || [0,0,0]}
+    get lookPitch() { return this._lookPitch}
+    get lookYaw() { return this._lookYaw}
+
+    lookTo(pitch, yaw) {
+        this._lookPitch = pitch;
+        this._lookYaw = yaw;
+        this.lastLookTime = this.time;
+        this.lastLookCache = null;
+        this.say("avatarLookTo", [pitch, yaw]);
+        this.say("lookGlobalChanged");
     }
 
     createOrbitControls( camera, renderer ) {
@@ -220,9 +248,17 @@ class AvatarPawn extends mix(Pawn).with(PM_Player, PM_Avatar, PM_ThreeVisible, P
     get lookGlobal() { 
         if (this.isMyPlayerPawn) {
             if(isTweening)return this.tweenCamera.matrixWorld.elements;
-            else if(isWalking)return this.global;
+            else if(isWalking)return this.walkLook;
             else {return this.orbitCamera.matrixWorld.elements;}
         }else return this.global;
+    }
+
+    get walkLook() {
+        const pitchRotation = q_axisAngle([1,0,0], this.lookPitch);
+        const m0 = m4_translation(this.lookOffset);
+        const m1 = m4_rotationQ(pitchRotation);
+        const m2 = m4_multiply(m1, m0);
+        return m4_multiply(m2, this.global);
     }
 
     update(time, delta) {
@@ -287,12 +323,24 @@ class AvatarPawn extends mix(Pawn).with(PM_Player, PM_Avatar, PM_ThreeVisible, P
         }
     }
 
+    thirdPerson(data){        
+        let z = this.lookOffset[2];
+        z += data/1000.0;
+        z = Math.min(4, Math.max(z,0));
+        this.lookOffset[1]=z/3;
+        this.lookOffset[2]=z;
+        this.lookTo(-z/8, 0);
+    }
+
     fadeNearby(){
-        let pawnManager = this.service("PawnManager");
-        let t = this.actor.translation;
+        let pawnManager = this.service("PawnManager"); 
         pawnManager.pawns.forEach(a => {
             if( a.avatar ){
-                let d = Math.min(4, v3_sqrMag(v3_sub(a.translation, t)))/4;
+                let m = this.lookGlobal; // camera location
+                let cv = new THREE.Vector3(m[12], m[13], m[14]);
+                m = a.global; // avatar location
+                let av = new THREE.Vector3(m[12], m[13], m[14])
+                let d = Math.min(1, cv.distanceToSquared(av)/3);
                 a.setOpacity(d);
             }
         });
@@ -401,4 +449,3 @@ StartWorldcore({
     view: MyViewRoot,
     tps:60
 });
-
