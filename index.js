2ba4c3270b7f77b273adfdd72fd8486596b1a44f
Yoshiki Ohshima <yoshiki.ohshima@acm.org>
2022-02-17 10:03:32 -0800

    update index.js

--- index.js
+++ index.js
@@ -10,14 +10,16 @@ import {
 import { myAvatarId, AvatarActor, AvatarPawn } from './src/DAvatar.js';
 //import { LightActor } from './src/DLight.js';
 import { KeyFocusManager, SyncedStateManager } from './src/text/text.js';
-import { DCardActor } from './src/DCard.js';
-//import { TextureSurface, VideoSurface } from './src/DSurface.js';
+import { DCardActor, VideoManager, DynaverseAppManager } from './src/DCard.js';
+import { DLight } from './src/DLight.js';
+import { WorldSaver } from './src/worldSaver.js';
 // apps -------------------------------------------
 import { MultiBlaster } from './apps/multiblaster.js';
-//import { SimpleCanvasSurface } from './apps/simpleCanvasSurface.js';
-//import { constructChess } from './apps/chess.js';
+import { BouncingBall } from './apps/bouncingBall.js';
+import { createChess } from './apps/chess.js';
 import { PerlinActor } from './apps/perlin.js';
-//import { constructBitcoin } from './apps/bitcoinTracker.js';
+import { BitcoinTracker, BitLogoCard, constructBitcoinTraker } from './apps/bitcoinTracker.js';
+import { DBarGraphCard } from './src/DBar.js';
 import { constructFlamingo } from './apps/flamingo.js';
 import JSZip from 'jszip';
 import * as fflate from 'fflate';
@@ -32,6 +34,161 @@ Constants.AvatarNames = [
     "generic/1", "generic/2", "generic/3", "generic/4", "generic/5", "generic/6",
     "alice", "newwhite", "fixmadhatter", "marchhare", "queenofhearts", "cheshirecat"
 ];
+
+Constants.DefaultCards = [
+    {
+        data: {
+            translation:[25, -90.5, -60],
+            scale:[200, 200, 200],
+            rotation: q_euler(0, Math.PI, 0),
+            layers: ['walk'],
+            type: "model",
+            model3d: "./assets/3D/Refinery.glb.zip",
+            singleSided: true,
+            shadow: true,
+        }
+    },
+    {
+        data: {
+            type: "lighting",
+            name: "Light",
+            className: "DLight",
+        }
+    },
+    {
+        data: {
+            className: "PerlinActor",
+            translation:[ 10, -2.75, -14],
+            rotation:[ 0, -0.7071068, 0, 0.7071068 ],
+        }
+    },
+    {
+        data: {
+            translation: [-4, -0.5, -6],
+            rotation: q_euler(0, Math.PI / 2, 0),
+            multiuser: true,
+            type: "text",
+            runs: [{text: "hello"}]
+        }
+    },
+    {
+        data: {
+            translation: [-4, -0.5, -12],
+            rotation: q_euler(0, Math.PI / 2, 0),
+            type: "model",
+            model3d: "./assets/avatars/generic/1.zip",
+            shadow: true,
+            singleSided: true,
+        }
+    },
+    {
+        data: {
+            translation: [-4, -0.5, -18],
+            rotation: q_euler(0, Math.PI / 2, 0),
+            scale: [4, 4, 4],
+            type: "shape",
+            textureType: "video",
+            textureURL: "./assets/videos/fromPCtoHMD.mp4",
+            shapeURL: './assets/SVG/credit-card.svg',
+            frameColor: 0x666666,
+            color: 0xffffff,
+            depth: 0.05,
+            fullBright: true
+        }
+    },
+    {
+        data: {
+            translation: [-4, -0.5, -24],
+            rotation: q_euler(0, Math.PI / 2, 0),
+            scale: [4, 4, 4],
+            type: "shape",
+            textureType: "texture",
+            textureURL: './assets/images/Colony.png',
+            shapeURL: './assets/SVG/credit-card.svg',
+            frameColor: 0x666666,
+            color: 0xffffff,
+            depth: 0.05,
+            fullBright: true
+        }
+    },
+    {
+        data: {
+            className: "MultiBlaster",
+            translation: [-4, -0.5, -30],
+            rotation: q_euler(0, Math.PI / 2, 0),
+            scale: [4, 4, 4],
+            layers: ['pointer'],
+            multiuser: true,
+            type: "app",
+            name: "MultiBlaster",
+            textureType: "canvas",
+            width: 1024,
+            height: 1024,
+            shapeURL: './assets/SVG/square.svg',
+            frameColor: 0x666666,
+            color: 0xffffff,
+            depth: 0.05,
+            fullBright: true
+        }
+    },
+    {
+        data: {
+            className: "BouncingBall",
+            translation: [-4, -0.5, -36],
+            rotation: q_euler(0, Math.PI / 2, 0),
+            scale: [4, 4, 4],
+            layers: ['pointer'],
+            multiuser: true,
+            type: "app",
+            name: "BouncingBall",
+            textureType: "canvas",
+            width: 1024,
+            height: 1024,
+            shapeURL: './assets/SVG/square.svg',
+            frameColor: 0x666666,
+            color: 0xffffff,
+            depth: 0.05,
+            fullBright: true,
+        }
+    },
+    /*
+    {
+        data: {
+            translation: [4, -0.5, -10],
+            rotation: q_euler(0, -Math.PI / 2, 0),
+            scale: [4, 4, 4],
+            layers: ['pointer'],
+            type: "code",
+            runs: [{text: `
+class Fly {
+    init() {
+        if (this.flying) {return;}
+        this.flying = true;
+        this.fly();
+    }
+
+    fly() {
+debugger;
+        this.future(20).call("Fly", "fly");
+        this.rotateTo(q_euler(0, this.now()/9000,0));
+    }
+}`}],
+            textWidth: 1024,
+            textHeight: 1024,
+        });            
+    */
+    {
+        data: {
+            rotation: q_euler(0, Math.PI / 2, 0),
+            offset: [8, 3, 0], // offset the flamingo model from the center
+            type: "model",
+            model3d: './assets/3D/Flamingo.glb.zip',
+            // actorCode: [shortId(code.id)]
+        }
+    }
+];
+
+let apps = new Map();
 
 function loadLoaders() {
     let libs = [
@@ -51,46 +208,21 @@ function loadLoaders() {
     }));
 }
 
+function shortId(id) {
+    let index = id.lastIndexOf("/");
+    if (index < 0) {return id;}
+    return id.slice(index + 1);
+}
+
 const tackOffset = 0.1;
 
 class MyAvatar extends AvatarActor {
     init(options) {
         this.avatarIndex = options.index; // set this BEFORE calling super. Otherwise, AvatarPawn may not see it
         super.init(options);
-        this.listen("addSticky", this.addSticky);
     }
 
     get pawn() {return MyAvatarPawn;}
-
-    addSticky(pe) {
-        let tackPoint = v3_add(pe.xyz, v3_scale(pe.normal, tackOffset));
-        let normal = [...pe.normal]; // clear up and down
-        normal[1] = 0;
-        let nsq = v3_sqrMag(normal);
-        let rotPoint;
-        if(nsq > 0.0001){
-            normal = v3_normalize(normal);
-            let theta = Math.atan2(normal[0], normal[2]);
-            rotPoint = q_euler(0, theta, 0);
-        } else {
-            rotPoint = this.rotation;
-            tackPoint[1] += 2;
-        }
-
-        DCardActor.create({
-            // cardShapeURL: `./assets/SVG/credit-card.svg`,
-            fullBright: true,
-            depth: 0.05,
-            shadow: true,
-            color:0xffffff, // white
-            frameColor:0x666666,
-            translation: tackPoint,
-            rotation: rotPoint,
-            text: "",
-            textWidth: 600,
-            textHeight: 600
-        });
-    }
 }
 
 MyAvatar.register('MyAvatar');
@@ -129,75 +261,120 @@ MyPlayerManager.register("MyPlayerManager");
 
 class MyModelRoot extends ModelRoot {
     static modelServices() {
-        return [MyPlayerManager];
+        return [MyPlayerManager, {service: DynaverseAppManager, options: {registry: apps}}];
     }
-    init(...args) {
-        super.init(...args);
+    init(options, persistentData) {
+        super.init(options);
 
-        //this.lights = LightActor.create();
+        this.ensurePersistenceProps();
+        this.subscribe(this.sessionId, "triggerPersist", "triggerPersist");
 
-        DCardActor.create({
-            translation:[25, -90.5, -60],
-            scale:[200, 200, 200],
-            rotation: q_euler(0, Math.PI, 0),
-            layers: ['walk'],
-            shadow: true,
-            model3d: "./assets/3D/Refinery.glb.zip",
-            modelType: "glb",
-        });
+        this.subscribe(this.id, "fileUploaded", "fileUploaded");
 
-        let tSurface = TextureSurface.create({url: './assets/images/Kay.jpg'});
-        let t2Surface = TextureSurface.create({url: './assets/images/Colony.png'});
-       
-        let vSurface = VideoSurface.create({url:'./assets/videos/fromPCtoHMD.mp4'});
-        let v2Surface = VideoSurface.create({url:'./assets/videos/Colony.mp4'});
-        
-        //let cSurface = SimpleCanvasSurface.create({name: 'SimpleCanvasSurface'});
-        let gSurface = MultiBlaster.create({name:'MultiBlaster'});
+        let appManager = this.service("DynaverseAppManager");
+        appManager.add(BitcoinTracker);
+        appManager.add(DBarGraphCard);
+        appManager.add(BitLogoCard);
+        appManager.add(MultiBlaster);
+        appManager.add(BouncingBall);
+        appManager.add(PerlinActor);
+        appManager.add(DLight);
 
-        let svgCards = [
-            'credit-card.svg', 'square.svg', 'credit-card.svg', 
-            'square.svg', 'square-full.svg', 'circle.svg', 'BitcoinSign.svg', 'rectangle.svg', 'cog.svg'];
-        let surfaces = [tSurface, tSurface, vSurface, gSurface, v2Surface, vSurface, cSurface];
-
-        for (let i = 0; i < 6; i++) {
-            DCardActor.create({
-                shapeURL: `./assets/SVG/${svgCards[i]}`,
-                surface: surfaces[i],
-                fullBright: surfaces[i].fullBright,
-                shadow: true,
-                depth: 0.05,
-                color:0xffffff, // white
-                frameColor:0x666666,
-                translation:[-4,-0.5, -6 * (i + 1)],
-                rotation: q_euler(0, Math.PI / 2, 0),
-                multiuser: true,
-                scale: [4,4,4],
-            });
+        if (persistentData) {
+            console.log("foo");
+            this.loadPersistentData(persistentData);
+            return;
         }
 
-        DCardActor.create({
-            fullBright: true,
-            depth: 0.05,
-            shadow: true,
-            color:0xffffff, // white
-            frameColor:0x666666,
-            translation:[5, -0.5, -10],
-            text: "Croquet is awesome!",
-            textWidth: 600,
-            textHeight: 600
-        });
+        this.load(Constants.DefaultCards, "1");
+        this.load(constructBitcoinTraker(), "1");
+    }
 
-        // demonstrates how to create an object
+    ensurePersistenceProps() {
+        if (!this.persistPeriod) {
+            let period = 1 * 60 * 1000;
+            this.persistPeriod = period;
+        }
+        if (this.lastPersistTime === undefined) {
+            this.lastPersistTime = 0;
+        }
 
-        this.perlin = PerlinActor.create(
-            {translation:[ 10, -2.75, -14],
-             rotation:[ 0, -0.7071068, 0, 0.7071068 ]}
-        );
-        //constructChess([8, -2.5, -30], [6,6,6]);
-//        constructBitcoin([-4,-0.5, -6 * 7], q_euler(0,Math.PI / 2,0), 4);
-//        constructFlamingo(3, 8);
-        this.subscribe(this.id, "fileUploaded", "fileUploaded");
+        if (this.persistRequested === undefined) {
+            this.persistRequested = false;
+        }
+    }
+
+    loadPersistentData({ _name, version, data }) {
+        try {
+            delete this.loadingPersistentDataErrored;
+            this.loadingPersistentData = true;
+
+            let saver = new WorldSaver(DCardActor);
+            let json = saver.parse(data);
+            this.load(json, version);
+        } catch (error) {
+            console.error("error in loading persistent data", error);
+            this.loadingPersistentDataErrored = true;
+        } finally {
+            delete this.loadingPersistentData;
+        }
+    }
+
+    savePersistentData() {
+        if (this.loadingPersistentData) {return;}
+        if (this.loadingPersistentDataErrored) {return;}
+        this.lastPersistTime = this.now();
+        let func = () => {
+            let name = this.sessionName || "Unknown";
+            let saver = new WorldSaver(DCardActor);
+            let json = saver.save(this);
+            return {name, version: "1", data: saver.stringify(json)};
+        };
+        this.persistSession(func);
+    }
+
+    load(data, version) {
+        if (version === "1") {
+            let map = new Map();
+            data.forEach(({id, data}) => {
+                let Cls;
+                if (data.className) {
+                    let appManager = this.service("DynaverseAppManager");
+                    Cls = appManager.get(data.className);
+                    data = {...data};
+                    delete data.className;
+                } else {
+                    Cls = DCardActor;
+                }
+                if (data.parent) {
+                    let parent = map.get(data.parent);
+                    data = {...data};
+                    data.parent = parent;
+                }
+                
+                let card = Cls.create(data);
+                if (id) { // could be the default content
+                    map.set(id, card);
+                }
+            });
+        }
+    }
+
+    triggerPersist() {
+        let now = this.now();
+        let diff = now - this.lastPersistTime;
+        let period = this.persistPeriod;
+        if (diff < period) {
+            if (!this.persistRequested) {
+                this.persistRequested = true;
+                this.future(period - diff).triggerPersist();
+            }
+            //console.log("persist not ready");
+            return;
+        }
+        this.lastPersistTime = now;
+        this.persistRequested = false;
+        this.savePersistentData();
     }
 
     fileUploaded(data) {
@@ -213,15 +390,17 @@ class MyModelRoot extends ModelRoot {
         let p = v3_add(v3_scale(n, 6),t);
 
         DCardActor.create({
-            shadow: true,
             translation: p,
             rotation: r,
+            type: "model",
             model3d: dataId,
             modelType: type,
+            shadow: true,
+            singleSided: true
         });
         this.publish(this.id, "fileLoadRequested", data);
+        this.publish(this.sessionId, "triggerPersist");
     }
-    
 }
 
 MyModelRoot.register("MyModelRoot");
@@ -233,7 +412,8 @@ class MyViewRoot extends ViewRoot {
             {service: ThreeRenderManager, options:{antialias:true}},
             AssetManager,
             KeyFocusManager,
-            SyncedStateManager
+            SyncedStateManager,
+            VideoManager, 
         ];
     }
     constructor(model) {
