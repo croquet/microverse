6fe3150eed279648c3a7be1c0ce2852d30e98c03
Yoshiki Ohshima <yoshiki.ohshima@acm.org>
2022-02-18 15:46:16 -0800

    wip: test version of toggle for flamingo

--- index.js
+++ index.js
@@ -164,11 +164,20 @@ class Fly {
         if (this.flying) {return;}
         this.flying = true;
         this.fly();
+        this.addEventListener("pointerDown", "toggle");
     }
 
     fly() {
+        if (!this.flying) {return;}
         this.future(20).call("Fly", "fly");
         this.rotateTo(WorldCore.q_euler(0, this.now()/9000,0));
+    }
+
+    toggle() {
+        this.flying = !this.flying;
+        if (this.flying) {
+           this.fly();
+        }
     }
 }`}],
             textWidth: 1024,
