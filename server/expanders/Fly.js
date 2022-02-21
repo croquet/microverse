7577be2bd701ef2083db194f3d57cbf81c4d1d22
Yoshiki Ohshima <yoshiki.ohshima@acm.org>
2022-02-21 10:12:38 -0800

    expander server

---
+++ server/expanders/Fly.js
@@ -0,0 +1,21 @@
+class Fly {
+    init() {
+        if (this.flying) {return;}
+        this.flying = true;
+        this.fly();
+        this.addEventListener("pointerDown", "toggle");
+    }
+
+    fly() {
+        if (!this.flying) {return;}
+        this.future(20).call("Fly", "fly");
+        this.rotateTo(WorldCore.q_euler(0, this.now()/9000,0));
+    }
+
+    toggle() {
+        this.flying = !this.flying;
+        if (this.flying) {
+           this.fly();
+        }
+    }
+}
---
+++ server/watch-server.js
@@ -0,0 +1,99 @@
+const chokidar = require('chokidar');
+const ws = require('ws');
+const fs = require('fs');
+const path = require('path');
+
+const wss = new ws.WebSocketServer({port: 9011});
+
+let files = {}; // {filename: content}
+let sentFiles = new Map();// {ws: {filename: content}}
+
+let watcher = chokidar.watch('./expanders/*.js' ,{
+    persistent: true,
+    ignored: /^[#]/
+});
+
+watcher.on('add', name => {
+    loadFile(name).then(() => {
+        sendAllFiles();
+    });
+});
+watcher.on('change', name => {
+    loadFile(name).then(() => {
+        sendAllFiles();
+    });
+});
+watcher.on('unlink', name => {
+    delete files[path.basename(name)];
+});
+
+function loadFile(name) {
+    let basename = path.basename(name);
+    return new Promise((resolve, reject) => {
+        fs.readFile(name, 'utf8', (err, data) => {
+            if (err) {
+                reject(err);
+            }
+            resolve(data);
+        });
+    }).then((file) => {
+        files[basename] = file;
+        console.log(files);
+    });
+}
+
+function heartbeat() {
+    this.isAlive = true;
+}
+
+const interval = setInterval(function ping() {
+    for (let ws of sentFiles.keys()) {
+        if (ws.isAlive === false) {
+            sentFiles.delete(ws);
+            return ws.terminate();
+        }
+        ws.isAlive = false;
+        ws.ping();
+    }
+}, 30000);
+
+wss.on('connection', (ws, request, client)  => {
+    sentFiles.set(ws, {});
+    ws.isAlive = true;
+    ws.on('pong', heartbeat);
+    sendFiles(ws);
+});
+
+function sendFiles(socket) {
+    let toSend = [];
+
+    let newKeys = Object.keys(files);
+    let sent = sentFiles.get(socket);
+    
+    let sentKeys = Object.keys(sent);
+    
+    newKeys.forEach((k) => {
+        if (files[k] && files[k] !== sent[k]) {
+            toSend.push({action: "add", name: k, content: files[k]});
+        }
+    });
+
+    sentKeys.forEach((k) => {
+        if (files[k] === undefined && sent[k]) {
+            toSend.push({action: "remove", name: k});
+        }
+    });
+    
+    socket.send(JSON.stringify(toSend));
+}
+
+function sendAllFiles() {
+    for (let k of sentFiles.keys()) {
+        sendFiles(k);
+    }
+}
+
+wss.on('close', function close() {
+    console.log("close", ws);
+    clearInterval(interval);
+});
