a88fa81e22ff5c036d99636785532b020566fab3
Yoshiki Ohshima <yoshiki.ohshima@acm.org>
2022-03-01 15:29:34 -0800

    workaround the setup ordering issue

--- expanders/BridgePawn.js
+++ expanders/BridgePawn.js
@@ -1,15 +1,16 @@
 class BridgePawn {
     setup() {
-        let d = this.actor.dimension;
+        let d = this.actor.dimension || 9; // hack until we resolve the setup ordering issue.
         if (!this.group) {
             this.group = new WorldCore.THREE.Group();
             this.group.name = "Bridge";
         }
 
         if (this.spheres) {
-            for (let j = 0; j < d; j++) {
+            for (let j = 0; j < this.spheres.length; j++) {
+                let row = this.spheres[j];
                 for (let i = 0; i < d; i++) {
-                    this.group.remove(this.spheres[j][i])
+                    this.group.remove(row[i]);
                 }
             }
         }
@@ -17,7 +18,7 @@ class BridgePawn {
         this.spheres = [...Array(d).keys()].map(j => {
             return [...Array(d).keys()].map(i => {
                 let geometry = new WorldCore.THREE.SphereGeometry(0.1, 16, 16);
-                let material = new WorldCore.THREE.MeshStandardMaterial({color: (i/d * 200) * 0x10000 + (j/d * 200)});
+                let material = new WorldCore.THREE.MeshStandardMaterial({color: (i / d * 200) * 0x10000 + (j / d * 200)});
                 let sphere = new WorldCore.THREE.Mesh(geometry, material);
                 this.group.add(sphere);
                 return sphere;
@@ -29,12 +30,17 @@ class BridgePawn {
     }
 
     updateDisplay() {
-        let d = this.actor.dimension;
         let ms = this.actor.ms;
-        for (let j = 0; j < d; j++) {
-            for (let i = 0; i < d; i++) {
-                let m = ms[j][i];
-                this.spheres[j][i].position.set(...m.p);
+        if (ms) {
+            for (let j = 0; j < ms.length; j++) {
+                let sRow = this.spheres[j];
+                let mRow = ms[j];
+                for (let i = 0; i < mRow.length; i++) {
+                    let m = mRow[i];
+                    let s = sRow[i];
+                    if (!s) {console.log("masses and spheres mismatch");}
+                    s.position.set(...m.p);
+                }
             }
         }
     }
