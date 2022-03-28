dc275359ed5b98d0dd42c208370a2671424ac25a
Yoshiki Ohshima <yoshiki.ohshima@acm.org>
2022-03-28 09:12:05 -0700

    fix spin.js

--- expanders/croquet/spin.js
+++ expanders/croquet/spin.js
@@ -1,35 +1,40 @@
-class SpinActor{
-    setup(){
-        this.listen("startSpinning", this.startSpinning);
-        this.listen("stopSpinning", this.stopSpinning);
+class SpinActor {
+    setup() {
+        this.scriptListen("startSpinning", this.startSpinning);
+        this.scriptListen("stopSpinning", this.stopSpinning);
     }
     
-    startSpinning(spin){
+    startSpinning(spin) {
         this.isSpinning = true;
-        this.qSpin = q_euler(0,spin,0);
+        this.qSpin = WorldCore.q_euler(0, spin, 0);
         this.doSpin();
     }
 
-    doSpin(){
-        if(this.isSpinning){
-            this.setRotation(q_multiply(this._rotation, this.qSpin));
+    doSpin() {
+        if(this.isSpinning) {
+            this.setRotation(WorldCore.q_multiply(this._rotation, this.qSpin));
             this.future(50).doSpin();
         }
     }
 
-    stopSpinning(){
+    stopSpinning() {
         this.isSpinning = false;
     }
 }
 
-class SpinPawn{
-    setup(){
+class SpinPawn {
+    setup() {
         this.addEventListener("pointerDown", "onPointerDown");
         this.addEventListener("pointerUp", "onPointerUp");
         this.addEventListener("pointerMove", "onPointerMove");
     }
 
-    onPointerDown(p3d){
+    theta(xyz) {
+        let local = this.world2local(xyz);
+        return Math.atan2(local[0], local[2]);
+    }
+
+    onPointerDown(p3d) {
         this.base = this.theta(p3d.xyz);
         this.deltaAngle = 0;
         this.say("stopSpinning");
@@ -37,15 +42,17 @@ class SpinPawn{
 
     onPointerMove(p3d) {
         let next = this.theta(p3d.xyz);
-        this.deltaAngle = (next - this.base)/2;
-        let qAngle = q_euler(0,this.deltaAngle,0);
-        this.setRotation(q_multiply(this._rotation, qAngle));
+        console.log(this.base, next);
+
+        this.deltaAngle = (next - this.base) / 2;
+        let qAngle = WorldCore.q_euler(0, this.deltaAngle, 0);
+        this.setRotation(WorldCore.q_multiply(this._rotation, qAngle));
     }
 
-    onPointerUp(p3d){
+    onPointerUp(p3d) {
         if(p3d.xyz){ // clean up and see if we can spin
             this.onPointerMove(p3d);
-            if(Math.abs(this.deltaAngle)>0.001){
+            if(Math.abs(this.deltaAngle) > 0.001) {
                 let a = this.deltaAngle;
                 a = Math.min(Math.max(-0.1, a), 0.1);
                 this.say("startSpinning", a);
@@ -59,3 +66,5 @@ export const spin = {
     actorExpanders: [SpinActor],
     pawnExpanders: [SpinPawn]
 }
+
+/* globals WorldCore */
