82fb7c988bdc96a8a1c3bb1cfaa883cccdebaa8c
Yoshiki Ohshima <yoshiki.ohshima@acm.org>
2022-04-07 15:05:12 -0700

    use microverse-file-server. create a better public library repo

---
+++ fix-index-html.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+DIST=${1}
+
+if [ -z ${DIST} ]
+then
+    echo "specify the destination dir"
+    exit 1;
+fi
+
+cat npm/index.html | sed  's:<script.*index.*\.js.*<\/script>:<script defer src="lib/index.js"><\/script>:' > ${DIST}/index.html
+mv  ${DIST}/lib/index-*.js ${DIST}/lib/index.js
--- webpack.public.js
+++ webpack.public.js
@@ -70,7 +70,6 @@ const config = {
                 },
                 { from: 'default.js', to: 'default.js' },
                 { from: 'test.js', to: 'test.js' },
-                { from: 'servers/file-server.js', to: 'servers/file-server.js' },
                 { from: path.resolve(__dirname, "staging")},
             ]
         }),
