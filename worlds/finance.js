525455e090fb295879a3f019df7cc14b40f9fdb0
Yoshiki Ohshima <yoshiki.ohshima@acm.org>
2022-04-15 09:16:25 -0700

    tweak values

--- worlds/finance.js
+++ worlds/finance.js
@@ -25,8 +25,8 @@ export function init(Constants) {
         {
             card: {
                 name:'wall st',
-                translation: [23, -24.47, 0],
-                rotation: [0, 0, 0],
+                translation: [0, -28, -22],
+                rotation: [0, -Math.PI / 2, 0],
                 dacaScale: [1, 1, 1],
                 scale:[30, 30, 30],
                 layers: ['walk'],
@@ -39,7 +39,7 @@ export function init(Constants) {
                 placeholder: true,
                 placeholderSize: [40, 1, 40],
                 placeholderColor: 0x808080,
-                placeholderOffset: [0, -0.463, 0],
+                placeholderOffset: [0, 0.4, 0],
             }
         },
         {
@@ -54,10 +54,10 @@ export function init(Constants) {
         },
         {
             card: {
-                translation: [16.563680868992712, -1.65688826723038, -11.402876582956996],
+                translation: [11.207345981534683, -5.141350571398242, -6.413481898133387],
+                scale: [33, 33, 33],
                 rotation: [0, 0, 0],
                 dataScale: [1, 1, 1],
-                scale: [30, 30, 30],
                 name: "/Kiosk_base.glb",
                 dataLocation: "3uwJbJUTFDggxMNRMBFWEcjbUbjLKF2bjBfjXQHwyj8wHQEBBQZPWloTHBkQBlsABlsWBxoEABABWxwaWgBaMhEjFwwZPAY0HiQWNy0RRjINFAMMAiA-IiwsR1ocGlsWBxoEABABWxgcFgcaAxAHBhBaIAQYQwMzJjhCGjpARBInQwMiHk1AQjwwOTwMTTs2RTMjHx4CAQ0lDy8nHloRFAEUWgM_PDcjRxMbEBsqDxciRgEjQAYWQ0YyIEJHPzgPIC0sGS8zLA0DQEZMECQ",
                 fileName: "/Kiosk_base.glb",
@@ -69,10 +69,10 @@ export function init(Constants) {
         },
         {
             card: {
-                translation: [16.563680868992712, -1.6263409205151738, -11.402876582956996],
+                translation: [11.207345981534683, -5.141350571398242, -6.413481898133387],
+                scale: [33, 33, 33],
                 rotation: [0, 0, 0],
                 dataScale: [1, 1, 1],
-                scale: [30, 30, 30],
                 name: "/Kiosk_screen_1.glb",
                 dataLocation: "36G5zWCENgH_RVPrDs97pgKCF6xyxo7wLbqPBNjQfh14XkJCRkUMGRlQX1pTRRhDRRhVRFlHQ1NCGF9ZGUMZcVJgVE9af0V3XWdVdG5SBXFOV0BPQWN9YW9vBBlfWRhVRFlHQ1NCGFtfVURZQFNERVMZY0dbAEBwZXsBWXkDB1FkAEBhXQ4DAX9zen9PDnh1BnBgXF1BQk5mTGxkXRlSV0JXGUNSB2ZFXkVTBFhdQUJMAFUOBxtRV3BgBxt-TlxQQHxZTFJlcHVSAgNVVwY",
                 fileName: "/Kiosk_screen_1.glb",
@@ -84,10 +84,10 @@ export function init(Constants) {
         },
         {
             card: {
-                translation: [16.563680868992712, -1.5480346712483157, -11.402876582956996],
+                translation: [11.207345981534683, -5.141350571398242, -6.413481898133387],
+                scale: [33, 33, 33],
                 rotation: [0, 0, 0],
                 dataScale: [1, 1, 1],
-                scale: [30, 30, 30],
                 name: "/Kiosk_screen_2.glb",
                 dataLocation: "3EcXJhKGfgQaI8FSUi9FNsqEYdzuj3hraTyE5C0SX6p4LTExNTZ_amojLCkgNmswNmsmNyo0MCAxaywqajBqAiETJzwpDDYELhQmBx0hdgI9JDM8MhAOEhwcd2osKmsmNyo0MCAxaygsJjcqMyA3NiBqEDQoczMDFghyKgpwdCIXczMSLn1wcgwACQw8fQsGdQMTLy4yMT0VPx8XLmohJDEkaj0BDgcQL3NyMQM1fXw8chw2HQMjcTMsLzEnIw0UISQkExEkNhIdAXMsFCY",
                 fileName: "/Kiosk_screen_2.glb",
@@ -99,10 +99,10 @@ export function init(Constants) {
         },
         {
             card: {
-                translation: [16.563680868992712, -1.4983451784677508, -11.402876582956996],
+                translation: [11.207345981534683, -5.141350571398242, -6.413481898133387],
+                scale: [33, 33, 33],
                 rotation: [0, 0, 0],
                 dataScale: [1, 1, 1],
-                scale: [30, 30, 30],
                 name: "/Kiosk_screen_3.glb",
                 dataLocation: "3WtpqdkBp4D2G7obtU4Ps42SDs3MJ2SilFvK0jjPThy8PyMjJyRteHgxPjsyJHkiJHk0JTgmIjIjeT44eCJ4EDMBNS47HiQWPAY0FQ8zZBAvNiEuIAIcAA4OZXg-OHk0JTgmIjIjeTo-NCU4ITIlJDJ4AiY6YSERBBpgOBhiZjAFYSEAPG9iYB4SGx4ubxkUZxEBPTwgIy8HLQ0FPHgzNiM2eGUPBC8vYhkAOWc1ECBkGwUEFjM2DjAkZxoDIghkLjUzbhYgPR0ULiMaJG8",
                 fileName: "/Kiosk_screen_3.glb",
