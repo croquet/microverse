7a8edd3e4dc31554cacb6a64040b795b13123243
Yoshiki Ohshima <yoshiki.ohshima@acm.org>
2022-04-16 13:26:28 -0700

    mvoe rotation to dataRotation for gears

--- worlds/default.js
+++ worlds/default.js
@@ -85,9 +85,9 @@ export function init(Constants) {
             card: {
                 name:'simple 3D model',
                 translation: [-4, -0.5, -18],
-                rotation: [Math.PI / 2, Math.PI / 2, 0],
                 type: "3d",
                 dataLocation: "./assets/3D/Gears.glb.zip",
+                dataRotation: [Math.PI / 2, Math.PI / 2, 0],
                 shadow: true,
                 singleSided: true,
             }
