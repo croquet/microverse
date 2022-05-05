15ca9edfbe1d9337bc01cc20fb0a0a1208ef9fa5
Yoshiki Ohshima <yoshiki.ohshima@acm.org>
2022-05-05 12:15:35 -0700

    add comments to cascade.js

--- behaviors/default/cascade.js
+++ behaviors/default/cascade.js
@@ -1,5 +1,25 @@
+/*
+  This Behavior for Actor creates a rigid body and a collider based on
+  rapierShape, rapierForce, and raperType properties, and add it to
+  the rapier-based simulation.
+
+  For information on Rapier, refer to this page:
+  https://rapier.rs/docs/user_guides/javascript/getting_started_js
+
+  Rapier provides bit-identical physical simulation so we use it on the Model/Actor side of Croquet.
+*/
+
 class CascadeActor {
     setup() {
+        /*
+          variable kinematic is initialized based on rapierType and
+          calls another behavior (Rapier)'s createRigidBoy method,
+          which in turn calls Rapier's method of the same name.
+
+          Variable RAPIER contains all exports from the rapier
+          packages. It is prefixed with Worldcore, which is the only
+          global variable visible to behavior code.
+        */
         let kinematic;
         let rapierType = this._cardData.rapierType;
         let rapierShape = this._cardData.rapierShape;
@@ -14,6 +34,11 @@ class CascadeActor {
         }
         this.call("Rapier$RapierActor", "createRigidBody", kinematic);
 
+        /*
+          variable cd (collider description) is initialized based on rapierShape and rapierSize,
+          and it is used for a call to createCollider.
+        */
+
         let cd;
         if (rapierShape === "ball") {
             let s = this._cardData.rapierSize || 1;
@@ -24,12 +49,31 @@ class CascadeActor {
             s = [s[0] / 2, s[1] / 2, s[2] / 2];
             cd = Worldcore.RAPIER.ColliderDesc.cuboid(...s);
         }
+
+        /*
+          Uncomment the shape === "cylinder" section to add cylinder type.
+          the ColliderDesc of Rapier tends to take the half-size; so [0.5, 0.5, 0.5] makes a [1, 1, 1]
+          cube.
+        */
+
+        /*else if (rapierShape === "cylinder") {
+            let s = this._cardData.rapierSize || [1, 1];
+            s = [s[1] / 2, s[0]];
+            cd = Worldcore.RAPIER.ColliderDesc.cylinder(...s);
+        }*/
+
         cd.setRestitution(this._cardData.rapierRestitution || 0.5);
         cd.setFriction(this._cardData.rapierFriction || 1);
         cd.setDensity(this._cardData.rapierDensity || 1.5);
 
+        /*
+          rapierSensor here adds intersectionEventHandler. Note that
+          this code (cascade.js) is in the user land; if you need to
+          have contactEventHandler for your application, you can
+          simply add it here.
+        */
+
         if (rapierSensor) {
-            console.log("sensor");
             this.registerIntersectionEventHandler("intersection");
             cd.setSensor(true);
             cd.setActiveEvents(Worldcore.RAPIER.ActiveEvents.CONTACT_EVENTS |
@@ -37,17 +81,33 @@ class CascadeActor {
         }
         this.call("Rapier$RapierActor", "createCollider", cd);
 
+        /*
+          If this is a regular moving object, add an event handler for pointerTap to invoke
+           the jolt method.
+        */
+
         if (!rapierType) {
             this.addEventListener("pointerTap", "jolt");
         }
 
+        /*
+          If the card spec has an initial rapierForce, put some force upon creation.
+        */
+
         if (rapierForce) {
             this.rigidBody.applyForce(rapierForce);
         }
+
+        /*
+          All movement of an object triggers "translating" event, and
+          it invokes the translated() method to implement the 'kill
+          plane'.
+        */
         this.listen("translating", "translated");
     }
 
     jolt() {
+        // Apply an upward force and random spin.
         let r = this.rigidBody;
         if (r) {
             r.applyForce({x: 0, y: 400, z: 0}, true);
@@ -56,21 +116,22 @@ class CascadeActor {
     }
 
     translated() {
-        // may not be a very efficient way to detect it
+        /*
+          if this object fell below, it kills itself.
+          destroy() is a method of the base CardActor. It invokes all destroy() methods of attached
+          behaviors. The Rapier behavior removes the rigidBody from the Rapier world.
+        */
         if (this._translation[1] < -10) {
             this.destroy();
         }
     }
 
-    registerContactEventHandler(methodName) {
-        let behavior = this._behavior;
-        let physicsManager = this.service("RapierPhysicsManager");
-        this.contactEventHandlerBehavior = `${behavior.module.name}$${behavior.$behaviorName}`;
-        this.contactEventHandlerMethod = methodName;
-        physicsManager.registerContactEventHandler(this._target);
-    }
-
     registerIntersectionEventHandler(methodName) {
+        /*
+          The CardActor has a method that invokes a behavior's method based on
+          `intersectionEventHandlerBehavior` and `intersectionEventHandlerMethod` values.
+          In this example, this is called from the above 'rapierSensor' case.
+        */
         let behavior = this._behavior;
         let physicsManager = this.service("RapierPhysicsManager");
         this.intersectionEventHandlerBehavior = `${behavior.module.name}$${behavior.$behaviorName}`;
@@ -78,7 +139,25 @@ class CascadeActor {
         physicsManager.registerIntersectionEventHandler(this._target);
     }
 
+    registerContactEventHandler(methodName) {
+        /*
+          The CardActor has a method that invokes a behavior's method based on
+          `contactEventHandlerBehavior` and `contactEventHandlerMethod` values.
+          this method is not used in this example but defined here for illustration purposes.
+        */
+        let behavior = this._behavior;
+        let physicsManager = this.service("RapierPhysicsManager");
+        this.contactEventHandlerBehavior = `${behavior.module.name}$${behavior.$behaviorName}`;
+        this.contactEventHandlerMethod = methodName;
+        physicsManager.registerContactEventHandler(this._target);
+    }
+
     intersection(card1, card2, intersecting) {
+        /*
+          With above set up This method will be called when two
+          rigidbodies, with at least one collider description with
+          active events intersects.
+        */
         console.log(card1, card2, intersecting);
         if (!intersecting) {return;}
         if (card1.id !== this.id) {
@@ -92,6 +171,16 @@ class CascadeActor {
 
 class CascadePawn {
     setup() {
+        /*
+          Creates a Three.JS mesh based on the specified rapierShape and rapierSize.
+
+          For a demo purpose, it does not override an existing shape
+          (by checking this.shape.children.length) so that the earth
+          shape created by FlightTracker behavior is preserved.
+
+          Uncomment the cyclinder case to add the cylinder shape.
+
+        */
         if (this.shape.children.length === 0) {
             let rapierShape = this.actor._cardData.rapierShape;
             if (rapierShape === "ball") {
@@ -109,6 +198,14 @@ class CascadePawn {
                 this.obj.castShadow = this.actor._cardData.shadow;
                 this.obj.receiveShadow = this.actor._cardData.shadow;
             }
+            /*else if (rapierShape === "cylinder") {
+                let s = this.actor._cardData.rapierSize || [1, 1];
+                let geometry = new Worldcore.THREE.CylinderGeometry(s[0], s[0], s[1], 20);
+                let material = new Worldcore.THREE.MeshStandardMaterial({color: this.actor._cardData.color || 0xff0000});
+                this.obj = new Worldcore.THREE.Mesh(geometry, material);
+                this.obj.castShadow = this.actor._cardData.shadow;
+                this.obj.receiveShadow = this.actor._cardData.shadow;
+            }*/
             this.shape.add(this.obj);
         }
         this.removeEventListener("pointerDoubleDown", "onPointerDoubleDown");
@@ -116,12 +213,25 @@ class CascadePawn {
     }
 }
 
+/*
+  This is used for the cube that sits above the inclined plane. It
+  calls spray() in a future loop at a 500 millisecond interval, and
+  create a new card by calling createCard().
+*/
 class SprayActor {
     setup() {
+        /*
+
+          Because of the life cycle of a behavior that means that the
+          setup() may be called multiple times on the same card, we tend to guard a property initialization with a conditional like this.
+        */
         if (this.running === undefined) {
             this.running = true;
             this.spray();
         }
+        /*
+          you can start and stop spray() loop by clicking the cube.
+        */
         this.addEventListener("pointerDown", "toggle");
     }
 
@@ -152,6 +262,11 @@ class SprayActor {
         if (!this.running) {return;}
         this.future(500).spray();
 
+        /*
+          For the demo purpose, we deine the demo in the global
+        coordinates. For a more controll example would add cards to an
+        owner card.
+        */
         const bt = [-20, 0, 53]; // bt for base translation
 
         let x = Math.random() * 200 - 100;
@@ -162,6 +277,9 @@ class SprayActor {
         let dice = Math.random();
 
         if (dice < 0.1) {
+            /*
+              The FlightTracker behavior is used, but without the "Elected" behavior, it does not start fetching the live data. It is used solely to create the Earth appearance.
+            */
             this.createCard({
                 name:"earth",
                 type: "object",
@@ -178,6 +296,15 @@ class SprayActor {
         }
 
         if (dice < 0.2) {
+            /*
+              Any card that has different behaviors can participate in the
+              simulation by having the "Rapier" and in this case the "Cascade"
+              behaviors.
+
+              The Slides behavior flips the textures specified in the
+              slides property, which holds a list of Croquet Data Id.
+            */
+
             this.createCard({
                 name:"wooden box",
                 type: "object",
@@ -199,6 +326,13 @@ class SprayActor {
             shape = "cuboid";
             size = [1, 1, 1];
         } else {
+            /*
+              uncomment to add cylinder to the simulation.
+            */
+            /*else if (dice < 0.8) {
+            shape = "cylinder";
+            size = [1, 1];
+            }*/
             shape = "ball";
             size = 2;
         }
@@ -224,6 +358,9 @@ class SprayActor {
     }
 }
 
+/*
+  Two behavior modules are exported from this file.  See worlds/default.js for their use.
+*/
 export default {
     modules: [
         {
