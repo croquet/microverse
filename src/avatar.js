b45b7e9d724ea0fab423de5b88208a3852ad2eeb
Vanessa Freudenberg <vanessa@codefrau.net>
2022-05-19 18:04:21 -0700

    hide own avatar in 1st person, and inside when going to 3rd person

--- src/avatar.js
+++ src/avatar.js
@@ -1133,13 +1133,13 @@ export class AvatarPawn extends mix(CardPawn).with(PM_Player, PM_SmoothedDriver,
         for (let [_viewId, a] of manager.players) {
             // a for actor, p for pawn
             let p = GetPawn(a.id);
-            if (a.follow) {
-                p.setOpacity(0); // get out of my way
-            } else if (a._playerId === presentationMode && v3_isZero(a.lookOffset)) {
-                p.setOpacity(0); // get out of my way
-            } else if (!this.actor.inWorld) {
-                p.setOpacity(1); // we are not even here
-            } else {
+            if (!this.actor.inWorld) {
+                p.setOpacity(1); // we are not even here so don't affect their opacity
+            } else if (a.follow) {
+                p.setOpacity(0); // never render followers
+            } else if ((p === this || a._playerId === presentationMode) && v3_isZero(a.lookOffset)) {
+                p.setOpacity(0); // never render me or leader in 1st person
+            } else { // fade based on their (or our own) distance between avatar and camera
                 let m = this.lookGlobal; // camera location
                 let cv = new THREE.Vector3(m[12], m[13], m[14]);
                 m = a.global; // avatar location
