2fb84f8ecd50b29195dcc874b357a9a4599438f8
Yoshiki Ohshima <yoshiki.ohshima@acm.org>
2022-06-15 13:36:28 -0700

    replace the factory model

--- worlds/factory.js
+++ worlds/factory.js
@@ -25,7 +25,7 @@ export function init(Constants) {
                 name:"world model",
                 layers: ["walk"],
                 translation: [-0, -5.234552517024578, -0],
-                dataLocation: "3LYn_S_GRNEPUqRtUPgMC4hhd_IwyziHCagqt7jO-gRoJDg4PD92Y2MqJSApP2I5P2IvPiM9OSk4YiUjYzljCygaLjUgBT8NJx0vDhQofws0LTo1OxkHGxUVfmMlI2IvPiM9OSk4YiElLz4jOik-PyljInoJPiQVLyk1HDwNdX19Gy8qOih_AxotBgkhJ3s_Ij0mFiE6GRMaDTs_L2MoLTgtYw41CCd5BRl0LhwEI34ENS0WNgI1eX4cCz17eQoLFBQqOnQDGwEuPiMmexU",
+                dataLocation: "34mlYnF8Yz2LiAu7ut9XWzOHWMrxM8wPmqOJlkqrOt2sXEBAREcOGxtSXVhRRxpBRxpXRltFQVFAGl1bG0Ebc1BiVk1YfUd1X2VXdmxQB3NMVUJNQ2F_Y21tBhtdWxpXRltFQVFAGlldV0ZbQlFGR1EbUgRcQXJkXV1tVldNU0dzWGZwZlddeldOGUYHZ3h9f21hUHVdZAFEfAVaDBtQVUBVG3FEZn9rWl9XdkYCf1lBXnNfGXtccgdcdloFcGxmXwx9XmxHeFBwVnx1QUM",
                 dataScale: [1.2, 1.2, 1.2],
                 fileName: "/Factory.glb",
                 modelType: "glb",
