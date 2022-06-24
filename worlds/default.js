4a6dfe92f770c6fbb80c80df90ff2a30cb6cc375
Vanessa Freudenberg <vanessa@codefrau.net>
2022-06-24 13:51:27 -0700

    Add Auggie Award to gallery

--- worlds/default.js
+++ worlds/default.js
@@ -72,6 +72,38 @@ export function init(Constants) {
                 shadow: true,
             }
         },
+        {
+            card: {
+                name: "auggie stand",
+                translation: [7.769621476269288, -1.7164662346793615, -14.695777508044646],
+                rotation: [0, -0.02446141974646361, 0, 0.999700774704105],
+                layers: ["pointer"],
+                name: "/AuggieStand.glb",
+                dataLocation: "3en5pHEDZi7EpsaJ-_yONogv4uVau9ZlIxyh1TH4AhAQDRERFRZfSkoDDAkAFksQFksGFwoUEAARSwwKShBKHzAREjUqHyMQKjZULgwCKD8MUFZcHSMhIl1VV0oMCksGFwoUEAARSwgMBhcKEwAXFgBLEwQLABYWBEoOPFcnN1A0MQFUNQJSCFArMhcyMj8hIx8RDCMBIB06LygUJgYULAEjFDBVSgEEEQRKUAoHFCdQMwYEESE1NCscPTckDggoDDUwDyIgFTFXVw5IDAhREDc6Jwc0MA",
+                fileName: "/AuggieStand.glb",
+                modelType: "glb",
+                shadow: true,
+                singleSided: true,
+                type: "3d",
+            }
+        },
+        {
+            card: {
+                name: "auggie award",
+                translation: [7.785789323121004, -0.4484673540691673, -14.785356996226062],
+                rotation: [0, -0.011707783853900597, 0, 0.9999314615498557],
+                layers: ["pointer"],
+                name: "/Auggie2.glb",
+                dataLocation: "35hI1OQ1NaqWki0Or-uHXAVM9vipZGVZIc_emQwLfCAcXUFBRUYPGhpTXFlQRhtARhtWR1pEQFBBG1xaGkAaT2BBQmV6T3NAemYEflxSeG9cAAYMTXNxcg0FBxpcWhtWR1pEQFBBG1hcVkdaQ1BHRlAbQ1RbUEZGVBpebAd3ZwBkYVEEZVICWAB7YkdiYm9xc09BXHNRcE1qf3hEdlZEfFFzRGAFGlFUQVQabV1-bFdNZAdnBAxCfnAYXk9kamVaAUVbBnt3QUBaWwUEfnhPb1RWfhhqDQ",
+                fileName: "/Auggie2.glb",
+                modelType: "glb",
+                license: "CC-BY",
+                attribution: "'Auggie Awards Trophy AWE 2022' by Kai Oldman derived from 'Auggie Awards Trophy AWE 2021' (https://skfb.ly/otrIP) by oriinbar is licensed under Creative Commons Attribution (http://creativecommons.org/licenses/by/4.0/).",
+                shadow: true,
+                singleSided: true,
+                type: "3d",
+            }
+        },
         /*
         {
             card: {
