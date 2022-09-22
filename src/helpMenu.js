a69911f3b8d72b81d5d737e08665900b464d7bb6
Jenn Evans <jenn.evans@croquet.io>
2022-09-22 15:48:05 -0700

    add other help and tips

--- src/helpMenu.js
+++ src/helpMenu.js
@@ -54,7 +54,7 @@ function createHelpMenu() {
         <div id="settings-title">Help</div>
         <div id="share-container">
           <div id="table-wrapper">
-            <div id="table-scroll">
+            <div id="table-scroll" id="scrollbar">
   
               <table class="help-tips">
                 <tr class="help-row">
@@ -79,6 +79,16 @@ function createHelpMenu() {
                   </td>
                 </tr>
                 <tr class="help-row">
+                <td>
+                  <p class="table-head">Manipulate</p>
+                  <p class="table-desc">Ctrl + click on an object to open that object's behavior properties. select from a premade menu or create your own (read docs)</p>
+                </td>
+                <td class="icon-column">
+                  <div class="icons"><img src="../assets/images/ctrlclick.png" />
+                  </div>
+                </td>
+              </tr>
+                <tr class="help-row">
                   <td>
                     <p class="table-head">Fullscreen</p>
                     <p class="table-desc">Make your browser fullscreen.</p>
@@ -98,6 +108,43 @@ function createHelpMenu() {
                     <p class="table-desc">Shows how many users in a world. Click to "gather" all users to you.</p>
                   </td>
                   <td class="icon-column"><i class="fa-light fa-users icons"></i></td>
+                </tr>
+                <tr class="help-row">
+                  <td>
+                    <p class="table-head">Import</p>
+                    <p class="table-desc">Import any of these formats from your desktop directly
+                    into the Microverse IDE. Either drag and drop, or click the import icon.</p>
+                  </td>
+                  <td class="icon-column">
+                    <div class="icons"><img src="../assets/images/import.png" /></div>
+                  </td>
+                </tr>
+                <tr class="help-row">
+                  <td>
+                    <p class="table-head">Connect</p>
+                    <p class="table-desc">With your favorite IDE open in the background, click connect to link the two for a live programming environment.</p>
+                  </td>
+                  <td class="icon-column">
+                    <div class="icons"><img src="../assets/images/connect.png" /></div>
+                  </td>
+                </tr>
+                <tr class="help-row">
+                  <td>
+                    <p class="table-head">Share</p>
+                    <p class="table-desc">Save your Microverse as a .vrse file to share or use the QR code to share the session with others.</p>
+                  </td>
+                  <td class="icon-column">
+                    <div class="icons"><img src="../assets/images/share.png" /></div>
+                  </td>
+                 </tr>
+                <tr class="help-row">
+                  <td>
+                    <p class="table-head">Settings</p>
+                    <p class="table-desc">Update your in-world nickname, select from default avatars or paste a link to your own.</p>
+                  </td>
+                  <td class="icon-column">
+                    <div class="icons"><img src="../assets/images/settings.png" /></div>
+                  </td>
                 </tr>
               </table>
             </div>
