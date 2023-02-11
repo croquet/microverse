026daa6b015a44a615c8cbe62233e6bb5b38d387
Vanessa Freudenberg <vanessa@codefrau.net>
2023-02-10 23:49:49 -0800

    make Spin behavior work in VR

--- behaviors/campus/spin.js
+++ behaviors/campus/spin.js
@@ -1,13 +1,14 @@
 // Spin
 // Copyright 2022 Croquet Corporation
 // Croquet Microverse
-// Adds a simple spin around y to a Tron
+// Adds a simple spin around y to a Card
 
-class SingleUserSpinActor {
+class SpinActor {
     setup() {
+        this.listen("newAngle", "newAngle");
         this.listen("startSpinning", "startSpinning");
         this.listen("stopSpinning", "stopSpinning");
-        this.listen("newAngle", "newAngle");
+        if (this._cardData.spin) this.startSpinning(this._cardData.spin);
     }
 
     startSpinning(spin) {
@@ -17,8 +18,9 @@ class SingleUserSpinActor {
     }
 
     doSpin() {
-        if(this.isSpinning) {
-            this.rotateTo(Microverse.q_multiply(this._rotation, this.qSpin));
+        if (this.isSpinning) {
+            let r = Microverse.q_multiply(this.rotation, this.qSpin);
+            this.set({rotation: r});
             this.future(50).doSpin();
         }
     }
@@ -27,19 +29,21 @@ class SingleUserSpinActor {
         this.isSpinning = false;
     }
 
-    newAngle(newAngle) {
-        this.publish("scope", "newAngle", newAngle);
+    newAngle({rotation, viewId}) {
+        this.set({rotation});
+        this.say("focus", viewId);
     }
 
     teardown() {
         delete this.isSpinning;
+        delete this.qSpin;
         this.unsubscribe(this.id, "startSpinning");
         this.unsubscribe(this.id, "stopSpinning");
         this.unsubscribe(this.id, "newAngle");
     }
 }
 
-class SingleUserSpinPawn {
+class SpinPawn {
     setup() {
         this.addEventListener("pointerDown", "onPointerDown");
         this.addEventListener("pointerUp", "onPointerUp");
@@ -55,48 +59,42 @@ class SingleUserSpinPawn {
         return this.actor.occupier == this.viewId;
     }
 
-    theta(xyz) {
-        // As the thing itself rotates, we need to get the "theta" in global.
-        let origin = this.translation;
-        return (Math.atan2(origin[2] - xyz[2], xyz[0] - origin[0]) + Math.PI * 2) % (Math.PI * 2);
-    }
-
     onPointerDown(p3d) {
-        if (!this.isSingleUser()) {
-            this.downP3d = p3d;
-            this.focusChanged();
-            return;
-        }
-        this.say("focus", this.viewId);
         this.downP3d = p3d;
+        // remember the down event for when we get the focus
+        if (this.isSingleUser()) {
+            this.say("focus", this.viewId);
+        } else {
+            this.focusChanged();
+        }
     }
 
     focusChanged() {
-        // console.log("focusChanged", this.actor.occupier);
         if (this.isSingleUser() && !this.hasFocus()) {
             if (this.downP3d) {this.onPointerUp(this.downP3d);}
             return;
         }
         let p3d = this.downP3d;
         if (!p3d) {return;}
-        this.moveBuffer = [];
         this.say("stopSpinning");
-        this._startDrag = p3d.xy;
-        this._baseRotation = this._rotation;
+        // we will rotate to an offset from this.baseRotation
+        this.baseRotation = this._rotation;
+        // the offset will be the distance of the raytip from a vertical plane along this ray
+        this.plane = this.planeFromRayAndAxis(p3d.ray, [0, 1, 0]);
+        // for kinetic spinning after letting go we need the last few moves
+        this.moveBuffer = [];
         let avatar = Microverse.GetPawn(p3d.avatarId);
         avatar.addFirstResponder("pointerMove", {}, this);
     }
 
     onPointerMove(p3d) {
-        // console.log("pointerMove", this.actor.occupier);
         if (this.isSingleUser() && !this.hasFocus()) {return;}
         if (!this.downP3d) {return;}
-        this.moveBuffer.push(p3d.xy);
-        this.deltaAngle = (p3d.xy[0] - this._startDrag[0]) / 2 / 180 * Math.PI;
-        let newRot = Microverse.q_multiply(this._baseRotation, Microverse.q_euler(0, this.deltaAngle, 0));
-        this.rotateTo(newRot);
-        this.say("newAngle", this.deltaAngle);
-        this.say("focus", this.viewId);
+        let offset = this.offsetFromRayTip(p3d.ray, this.downP3d.distance);
+        this.moveBuffer.push(offset);
+        const {q_multiply, q_euler} = Microverse;
+        let rotation = q_multiply(this.baseRotation, q_euler(0, offset, 0));
+        this.say("newAngle", {rotation, viewId: this.viewId});
         if (this.moveBuffer.length >= 3) {
             setTimeout(() => this.shiftMoveBuffer(), 100);
         }
@@ -108,14 +106,15 @@ class SingleUserSpinPawn {
 
     onPointerUp(p3d) {
         this.say("unfocus", this.viewId);
+        let distance = this.downP3d.distance;
         this.downP3d = null;
+        this.baseRotation = null;
         let avatar = Microverse.GetPawn(p3d.avatarId);
         avatar.removeFirstResponder("pointerMove", {}, this);
         if (this.isSingleUser() && !this.hasFocus()) {return;}
-        this.moveBuffer.push(p3d.xy);
 
-        this._startDrag = null;
-        this._baseRotation = null;
+        let offset = this.offsetFromRayTip(p3d.ray, distance);
+        this.moveBuffer.push(offset);
 
         if (this.moveBuffer.length < 3) {return;}
 
@@ -123,17 +122,36 @@ class SingleUserSpinPawn {
 
         let signs = new Set();
         for (let i = 0; i < this.moveBuffer.length - 1; i++) {
-            signs.add(Math.sign(this.moveBuffer[i + 1][0] - this.moveBuffer[i][0]));
+            signs.add(Math.sign(this.moveBuffer[i + 1] - this.moveBuffer[i][0]));
         }
         if (signs.has(-1) && signs.has(1)) {return;}
 
-        this.deltaAngle = (this.moveBuffer[this.moveBuffer.length - 1][0] - this.moveBuffer[0][0]) / 2 / 180 * Math.PI;
+        let deltaAngle = (this.moveBuffer[this.moveBuffer.length - 1] - this.moveBuffer[0]);
 
-        if (Math.abs(this.deltaAngle) > 0.01) {
-            let a = this.deltaAngle;
+        if (Math.abs(deltaAngle) > 0.01) {
+            let a = deltaAngle;
             a = Math.min(Math.max(-0.1, a), 0.1);
             this.say("startSpinning", a);
         }
+    }
+
+
+    offsetFromRayTip(ray, distance) {
+        const {v3_add, v3_scale, v3_dot} = Microverse;
+        // tip is at the end of the current ray at same distance as down ray
+        let tip = v3_add(ray.origin, v3_scale(ray.direction, distance));
+        // offset is tip's distance from the plane
+        let offset = v3_dot(this.plane.normal, tip) - this.plane.constant;
+        return offset;
+    }
+
+    planeFromRayAndAxis(ray, axis) {
+        const {v3_normalize, v3_dot, v3_cross} = Microverse;
+        // ray and axis form the plane, so its normal is perpendicular to both
+        let normal = v3_normalize(v3_cross(ray.direction, axis));
+        // ray origin is on the plane
+        let constant = v3_dot(normal, ray.origin);
+        return {normal, constant};
     }
 
     teardown() {
@@ -146,9 +164,9 @@ class SingleUserSpinPawn {
 export default {
     modules: [
         {
-            name: "SingleUserSpin",
-            actorBehaviors: [SingleUserSpinActor],
-            pawnBehaviors: [SingleUserSpinPawn]
+            name: "Spin",
+            actorBehaviors: [SpinActor],
+            pawnBehaviors: [SpinPawn]
         }
     ]
 }
