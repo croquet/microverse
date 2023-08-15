df4bac2b85f17d72872a66d37963f1c54f6a4819
Yoshiki Ohshima <yoshiki.ohshima@acm.org>
2023-08-15 16:38:20 -0700

    update eslint config for typescript case

--- .eslintrc.js
+++ .eslintrc.js
@@ -76,5 +76,28 @@ module.exports = {
                          ],
 
       "import/first": [ "off" ],
-  }
+  },
+
+  "overrides": [
+      {
+        "files": ["**/*.ts"],
+        "env": { "browser": true, "es6": true, "node": true },
+        "extends": [
+          "eslint:recommended",
+          "plugin:@typescript-eslint/eslint-recommended",
+          "plugin:@typescript-eslint/recommended"
+        ],
+        "globals": { "Atomics": "readonly", "SharedArrayBuffer": "readonly" },
+        "parser": "@typescript-eslint/parser",
+        "parserOptions": {
+          "ecmaVersion": 2018,
+          "sourceType": "module",
+        },
+        "plugins": ["@typescript-eslint"],
+        "rules": {
+            "@typescript-eslint/no-explicit-any": 0,
+            'prefer-const': 'off'
+        }
+      }
+  ]
 };
